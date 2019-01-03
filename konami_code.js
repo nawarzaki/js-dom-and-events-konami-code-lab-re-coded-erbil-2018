65864aac2e372162e6b066b80df39a7d0e10719f
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-01-03 14:07:38 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,30 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0;
+let index = 0
 
 function init() {
   // Write your JavaScript code inside the init() function
-
-}
-
-const alphabet = [65, 66, 67];
- 
-// Keep track of index outside of the event handler.
-let index = 0;
- 
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
- 
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
- 
-    if (index === alphabet.length) {
-      alert("Hurray!");
- 
+
+    if (index === code.length) {
+      alert("congratulation!");
+
       index = 0;
     }
   } else {
     index = 0;
   }
+
 }
+
+document.body.appendChild(code)
+
+addEventListener('keydown', init)
+
